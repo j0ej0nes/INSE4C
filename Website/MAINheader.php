a3b8bfbe1d428f93680b6d24a7d21183070ea560
MaxBidz <up619997@myport.ac.uk>
2014-01-08 12:21:49 +0000

    Create MAINheader.php

---
+++ Website/MAINheader.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>INSE4C</title>
+<meta name="author" content="INSE4c">
+<link rel="stylesheet" type="text/css" href="Style.css">
+
+</head>
+<?php
+include("config.php")
+?>
+
+<body>
+        <div id="box">
+
+<article>
+
+
+<section class="header"><h1>INSE4C</h1></section>
+                        
+                                                <nav>
+                                                        <ul>
+                                                                
+                                                                <li><a href="WBT.php">WBT</a></li>
+                                                                <li><a href="PERT.php">PERT</a></li>
+																<li><a href="GANT.php">GANT</a></li>
+                                                                
+                                                                
+                                                                
+                                                        </ul> 
+                                                </nav>
+</article>
+                </div>
