02b254bb20b02f82604284661ed353644f5e4069
MaxBidz <up619997@myport.ac.uk>
2014-01-08 12:19:16 +0000

    Create WBT.php

---
+++ Website/WBT.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+
+<meta charset="UTF-8">
+<title>Work Breakdown Tree</title>
+<meta name="author" content="INSE4c">
+<link rel="stylesheet" type="text/css" href="Style.css">
+
+</head>
+
+
+<body>
+
+
+</body>
+
+<p class="level1">
+<INPUT TYPE = "Text" VALUE ="How many tasks in level 1?" NAME = "level1">
+<INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Submit">
+</p>
+
+<p class="level2">
+<INPUT TYPE = "Text" VALUE ="How many tasks in level 2?" NAME = "level2">
+<INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
+</p>
+
+<p class="level3">
+<INPUT TYPE = "Text" VALUE ="Project Name" NAME = "level3">
+<INPUT TYPE = "Submit" Name = "Submit3" VALUE = "Submit">
+</p>
+<?php
+include("WBTheader.php")
+
+//$x = $_POST['0'];
+//print
+?>
