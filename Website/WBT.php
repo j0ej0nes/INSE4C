acb7319cfa1c24a250b327f4293edf2813c426b8
MaxBidz <up619997@myport.ac.uk>
2014-04-24 10:39:41 +0100

    Update WBT.php

--- Website/WBT.php
+++ Website/WBT.php
@@ -1,33 +1,89 @@
-<?php
-include("header.php")
-?>
-<?php
-require('config.php');
-?>
+<html lang="en">
 
-
+<head>
+	<title>WBT chart</title>
+	<link rel="stylesheet" type="text/css" href="wbt.css">
+<!-- this code is here to load library for the drag function (for the box)-->
+	        <script src="http://www.google.com/jsapi" type="text/javascript"></script>
+        <script type="text/javascript" >
+           google.load("jquery", "1.6.3");
+           google.load("jqueryui", "1.8.16");
+        </script>
+        <link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/themes/base/jquery-ui.css"/>
+        <script src="dragndrop.js" type="text/javascript"></script>
+<!--library code ends here-->	
+</head>
 <body>
-
-
-</body>
-<fieldset><legend><span> </span></legend>
-<p class="level1">
-<INPUT TYPE = "Text" VALUE ="How many tasks in level 1?" NAME = "level1">
-<INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Submit">
-</p>
-
-<p class="level2">
-<INPUT TYPE = "Text" VALUE ="How many tasks in level 2?" NAME = "level2">
-<INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
-</p>
-
-<p class="level3">
-<INPUT TYPE = "Text" VALUE ="Project Name" NAME = "level3">
-<INPUT TYPE = "Submit" Name = "Submit3" VALUE = "Submit">
-</p>
-</fieldset>
 <?php
-
-//$x = $_POST['0'];
-//print
+	include("MAINheader.php")
 ?>
+	<script type="text/javascript" src="wz_dragdrop.js"></script>
+<div id="wrapper">
+	<div id="menu">
+		<h2>main menu</h2><br>
+			<a>Box</a>
+				<div id="drag1" class="drag">
+
+
+					<form id="draggable2" action="demo_form.asp">
+						<label for="male">Task</label>
+							<textarea id="usrname" name="usrname" rows="3" style="font-family: Arial" style="font-size: 22px" ></textarea><br>
+					</form> 
+				</div>
+
+
+				<a>Arrow</a>
+						<div id="arrow">
+							<img src="arrow-right.jpg" id="arrow2" class="home" src="img_trans.gif">
+							<img src="arrow-left.jpg" id="arrow3" class="home" src="img_trans.gif">
+							<img src="arrow-up.jpg" id="arrow4" class="home2" src="img_trans.gif">
+							<img src="arrow-down.jpg" id="arrow5" class="home2" src="img_trans.gif">
+							<button onclick="myFunction()">Arrow help</button>
+						<script>
+							function myFunction()
+							{
+							alert("hold 'shift' and drag to resize arrows");
+							}
+						</script>
+
+
+
+	<button onclick="printFunction()">Print</button>
+
+<script>
+function printFunction()
+{
+window.print();
+}
+</script>
+
+
+
+
+
+						</div>
+	</div>
+							<h1 style="text-align:center; ">Work Breakdown Tree</h1>
+							<link href="style.css">
+								<div class="col" id ="droppable">
+
+								</div>
+]
+
+
+
+
+
+	<script type="text/javascript">
+	<!--
+	SET_DHTML("arrow2"+COPY+50+RESIZABLE+CURSOR_HAND,"arrow3"+COPY+50+RESIZABLE+CURSOR_HAND,"arrow4"+COPY+50+RESIZABLE+CURSOR_HAND,"arrow5"+COPY+50+RESIZABLE+CURSOR_HAND);
+	//-->
+	</script>
+
+	<div id="footer">INSE4C
+			</div>
+	
+	
+</div>
+</body>
+</html>
